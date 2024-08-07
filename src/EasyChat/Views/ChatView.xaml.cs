096e9a60535a05bfd3388a0cb738b26a656ed7ba
ftq <ftq62017@126.com>
2024-08-07 09:36:42 +0800

    添加聊天界面，MenuButton样式存在问题

---
+++ src/EasyChat/Views/ChatView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace EasyChat.Views
+{
+    public partial class ChatView : Window
+    {
+        public ChatView()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+
+        }
+
+        bool IsMaximized = false;
+        private void Boreder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ClickCount ==2)
+            {
+                if(IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1250;
+                    this.Height = 830;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    IsMaximized = true;
+                }
+            }
+        }
+    }
+}
